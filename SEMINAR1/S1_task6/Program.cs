9db1091267ac3efcd3c49b0f2bf9b54749d27bba
Shamil <karimshg@gmal.com>
2022-06-24 15:31:45 -0700

    add task6

---
+++ SEMINAR1/S1_task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+
+Console.Write("Input a number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num%2==0)
+    Console.WriteLine("number is even.");
+else
+    Console.WriteLine("number is odd");
+
