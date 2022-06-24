8e295fd17b493b4c2c770cc68676019a806c8e87
Shamil <karimshg@gmal.com>
2022-06-24 16:15:12 -0700

    add task8

---
+++ SEMINAR1/S1_task8/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1;
+Console.WriteLine("Input a number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = 1;
+
+while (num2 < num1)
+    {
+	if (num2 % 2 == 0)
+    {
+	     Console.Write(num2 + " ");
+	}
+	 num2 ++;
+	}
+
+
+
