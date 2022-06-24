def0d8eb10a0e8d7656c4d49864a75da58b34ec0
Shamil <karimshg@gmal.com>
2022-06-24 14:56:59 -0700

    add SEMINAR1

---
+++ SEMINAR1/S1_task2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int num1, num2;
+
+Console.Write("Input a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2)
+    Console.WriteLine("First number - max, second number - min.");
+else
+    Console.WriteLine("First number - min, second number - max.");
